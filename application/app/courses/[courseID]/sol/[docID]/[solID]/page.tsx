d6062342d6ff5677b2c121882cce50c41960529b
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-06-19 01:46:56 +0500

    Structure for Routing of Documents Done

---
+++ application/app/courses/[courseID]/sol/[docID]/[solID]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Document Solution Page</div>;
+};
+
+export default page;
---
+++ application/app/courses/[courseID]/sol/[docID]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      All Available Solution Page for Specific Document that has been clicked
+    </div>
+  );
+};
+
+export default page;
