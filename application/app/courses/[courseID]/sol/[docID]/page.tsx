f99341fe713c2d72d72ced4948bc69655e57e69b
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-06-20 10:21:42 +0500

    Added Document Solutions Page

--- application/app/courses/[courseID]/sol/[docID]/page.tsx
+++ application/app/courses/[courseID]/sol/[docID]/page.tsx
@@ -1,11 +1,46 @@
+import { BreadcrumbWithDropdown } from "@/components/BreadCrumb";
+import { SolutionCard } from "@/components/SolutionCard";
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
-const page = () => {
+const Page = () => {
   return (
-    <div>
-      All Available Solution Page for Specific Document that has been clicked
-    </div>
+    <>
+      <div className="flex flex-col sm:flex-row justify-between items-center p-4 bg-gray-100 dark:bg-gray-900">
+        <BreadcrumbWithDropdown />
+        <div className="relative w-full sm:w-auto mt-2 sm:mt-0">
+          <input
+            type="text"
+            placeholder="Document/Instructor/Contributor Name"
+            className="pl-10 pr-4 py-2 w-full sm:w-80 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 dark:bg-gray-700 dark:text-gray-200 dark:border-gray-600 dark:focus:border-gray-400"
+          />
+          <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 dark:text-gray-300" />
+        </div>
+      </div>
+      <div className="pt-4">
+        <div className="mt-2 pt-2 pb-4 border-2 border-gray-300 dark:border-gray-700 rounded-lg shadow-lg bg-white dark:bg-gray-800">
+          <h1 className="pl-4 text-2xl font-bold mb-4 text-gray-800 dark:text-gray-200">
+            Available Solutions for ......
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {Array.from({ length: 5 }, (_, index) => (
+              <SolutionCard
+                key={index}
+                documentName="Assignment 4 Solution"
+                instructorName="Mam Zahid"
+                semester="Spring 2024"
+                uploadDate="12-NOV-2023"
+              />
+            ))}
+          </div>
+        </div>
+        <div className="p-4 flex justify-center">
+          <Button variant="destructive">Load More</Button>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default page;
+export default Page;
--- application/components/SolutionCard.tsx
+++ application/components/SolutionCard.tsx
@@ -2,12 +2,10 @@
 
 import * as React from "react";
 import { useRouter, usePathname } from "next/navigation";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -15,38 +13,31 @@ import { Label } from "@/components/ui/label";
 import { IoMdArrowDroprightCircle } from "react-icons/io";
 import { FaLocationArrow } from "react-icons/fa";
 
-export function DocumentCard({
+export function SolutionCard({
   documentName,
   instructorName,
   semester,
   uploadDate,
-  solutionsCount,
 }: {
   documentName: string;
   instructorName: string;
   semester: string;
   uploadDate: string;
-  solutionsCount: string;
 }) {
   const router = useRouter();
   const currentPath = usePathname();
 
   const constructUrl = (basePath: string, type: string) => {
     const pathSegments = currentPath.split("/").filter(Boolean);
-    return `/${pathSegments[0]}/${pathSegments[1]}/${type}/${documentName}`;
+    return `/${pathSegments[0]}/${pathSegments[1]}/${type}/${documentName}/${documentName.split(" ").join("-")}`;
   };
 
   const handleClickDocument = () => {
-    router.push(constructUrl(currentPath, "doc"));
-  };
-
-  const handleClickSolution = (event: React.MouseEvent) => {
-    event.stopPropagation();
     router.push(constructUrl(currentPath, "sol"));
   };
 
   return (
-    <Card className="max-w-[300px] w-full shadow-lg hover:shadow-2xl transition-shadow duration-300 cursor-pointer transform hover:-translate-y-1 hover:scale-105 m-4 bg-white dark:bg-gray-800">
+    <Card className="max-w-[320px] w-full shadow-lg hover:shadow-2xl transition-shadow duration-300 cursor-pointer transform hover:-translate-y-1 hover:scale-105 m-4 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-lg">
       <CardHeader>
         <div
           className="flex justify-between items-center cursor-pointer bg-slate-200 dark:bg-gray-700 p-4 rounded-lg transition-colors duration-300 hover:bg-slate-300 dark:hover:bg-gray-600"
@@ -76,13 +67,13 @@ export function DocumentCard({
               Uploaded on: {uploadDate}
             </Label>
           </div>
+          <div className="flex flex-col space-y-1.5">
+            <Label className="text-gray-700 dark:text-gray-400">
+              Uploaded by: Deepak Kumar
+            </Label>
+          </div>
         </div>
       </CardContent>
-      <CardFooter className="flex flex-col gap-1">
-        <Button onClick={handleClickSolution} className="text-sm self-start">
-          Available Solutions: {solutionsCount}
-        </Button>
-      </CardFooter>
     </Card>
   );
 }
